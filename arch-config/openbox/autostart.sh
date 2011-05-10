53c3e8b83f427b7da53c3f7ea9b2284e2971cfea
Joyce Tipping <joyce@joycetipping.com>
2011-05-10 08:22:41 -0600

    Configuring desktop

--- arch-config/openbox/autostart.sh
+++ arch-config/openbox/autostart.sh
@@ -1,41 +1,17 @@
-## Openbox autostart.sh
-## ====================
-## When you login to your CrunchBang Openbox session, this autostart script 
-## will be executed to set-up your environment and launch any applications
-## you want to run at startup.
-##
-## More information about this can be found at:
-## http://openbox.org/wiki/Help:Autostart
-##
-## If you do something cool with your autostart script and you think others
-## could benefit from your hack, please consider sharing it at:
-## http://crunchbanglinux.org/forums/
-##
-## Have fun! :)
+# Openbox autostart
+# Based heavily on the one that ships with CrunchBang linux
+#
+# More information about this can be found at:
+# http://openbox.org/wiki/Help:Autostart
 
-~/bin/setup-background
-~/bin/setlayout 0 6 6 0
+./setup-background                  # A wrapper for nitrogen
+./setlayout 0 6 6 0                 # Set up virtual desktops
 
-## Start session manager
-lxsession &
+synergys -c ~/.synergy.conf
 
-## Enable power management
-xfce4-power-manager &
+lxsession &                             # Start session manager
+xfce4-power-manager &                   # Enable power management
+cairo-compmgr &                         # Enable Eyecandy
 
-## Launch panel
-(sleep 2s && tint2) &
-
-## Enable Eyecandy - off by default, uncomment one of the commands below.
-## Note: cairo-compmgr prefers a sleep delay, else it tends to produce
-## odd shadows/quirks around tint2 & Conky.
-cairo-compmgr &
-#cb-compmgr --xcompmgr & 
-
-## Detect and configure touchpad. See 'man synclient' for more info.
-if egrep -iq 'touchpad' /proc/bus/input/devices; then
-    synclient VertEdgeScroll=1 &
-    synclient TapButton1=1 &
-fi
-
-## Start Conky after a slight delay
-(sleep 3s && conky -q) &
+(sleep 2s && tint2) &                   # Panel
+(sleep 3s && conky -q) &                # Notifications
